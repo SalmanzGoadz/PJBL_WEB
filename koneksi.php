5e953b06c239c1f6a954abd859fc2e952768ef88
BlueTedV <zudrakha2@gmail.com>
2025-05-15 19:06:36 +0700

    Deskripsi

--- koneksi.php
+++ koneksi.php
@@ -1,17 +1,3 @@
-<<<<<<< HEAD
-<?php 
-
-$host="localhost";
-$user="root";
-$pass="";
-$db="kora_db";
-$conn=new mysqli($host,$user,$pass,$db);
-
-if($conn->connect_error){
-    http_response_code(500);
-    die("gagal konek ke db: " . $conn->connect_error);
-}
-=======
 <?php 
 
 $host="localhost";
@@ -23,5 +9,4 @@ $conn=new mysqli($host,$user,$pass,$db);
 if($conn->connect_error){
     echo"gagal konek ke db".$conn->connect_error;
 }
->>>>>>> cd0a9dd9413469b337ef770834175199865ee366
 ?>
